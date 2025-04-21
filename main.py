78134d36cdee752d560fb414660c0cdc6d2e2ded
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2025-04-21 13:52:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sqlite3
 from sqlite3 import Cursor
 
-from flask import Flask
+from flask import Flask, redirect, render_template
 from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
@@ -20,11 +20,41 @@ def execute(cursor: Cursor, query: str):
         chatdb.execute(query)
         chatsconn.commit()
 
-'''
+# Index BP
 @app.route('/')
 def index():
     return 'Hello from Flask!'
 
+# Login + Signup BP
+@app.route('/login')
+def login():
+    return 'Login'
+
+@app.route('/signup')
+def signup():
+    return 'Signup'
+
+# Chats BP: redirect (parent)
+@app.route('/chats')
+def chats_redirect_to_all_chats():
+    return redirect('/chats/all')
+
+# All chats BP (child of above)
+@app.route('/chats/all')
+def all_chats():
+    return 'All chats'
+
+# Room chat BP (child of above above)
+@app.route('/chat/room/<chatname>')
+def chatroom(chatname):
+    return chatname
+
+# DM chat BP (child of above above above)
+@app.route('/chat/direct/<username>')
+def direct_chat(username):
+    return username
+
+
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
-'''
+  app.run(host='0.0.0.0', port=8080)
+

5017a96353b63fffba016c54a51fd0571ee7c08a
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2025-04-27 01:26:55 +0000

    Created the chat database

---
+++ initdb.py
@@ -0,0 +1,12 @@
+from main import *
+
+
+
+execute(userdb, '''
+CREATE TABLE chatdb (
+id INTEGER PRIMARY KEY,
+chatname varchar(255) UNIQUE,
+chatmessages TEXT NOT NULL,
+chatdata TEXT NOT NULL
+)
+''')

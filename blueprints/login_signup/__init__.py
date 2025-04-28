4fb4cbb05e7b2552bf6aa11cd652af329831ef24
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2025-04-28 14:23:55 +0800

    Update __init__.py

--- blueprints/login_signup/__init__.py
+++ blueprints/login_signup/__init__.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, redirect, render_template
+from flask import Blueprint, redirect, render_template, session
+from werkzeug.security import generate_password_hash, check_password_hash
 
 login_signup = Blueprint(
   'login_signup', 
@@ -10,6 +11,15 @@ login_signup = Blueprint(
 # Login + Signup BP
 @login_signup.route('/login', methods=['GET', 'POST'])
 def login():
+  if request.method == 'POST':
+    username = request.form['username']
+    password = request.form['password']
+    hashed_password_from_user = execute()
+    if check_password_hash(password, hashed_password_from_user):
+      session['username'] = username
+      return redirect("/chats/all")
+    else:
+      return render_template('login/login.html'), flash('danger|Password is incorrect.')
   return render_template('login/login.html')
 
 @login_signup.route('/signup', methods=['GET', 'POST'])

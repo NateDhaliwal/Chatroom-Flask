30100086b810a040fb2cee685573611c2816f8fb
= <nathaniel.shaan@gmail.com>
2025-06-22 18:28:00 +0800

    Created and migrated SQLAlchemy tables for user, chat, message, member

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-from blueprints.login_signup import login_signup
+from views.login_signup import login_signup
 
 app = Flask(__name__, static_folder='static')
 
@@ -10,9 +11,10 @@ app.secret_key = __import__("secrets").token_hex(16)
 # Register all Blueprints
 app.register_blueprint(login_signup)
 
-sqlaldb = SQLAlchemy()
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlalchemy.db"
-sqlaldb.init_app(app)
 
 # Index BP
 @app.route('/')

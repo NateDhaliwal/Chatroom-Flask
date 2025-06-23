d03bf25601a7bce11f2bc4204d8e2c9aadc028b0
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-06-23 13:29:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from models import db, migrate
 
 from views.login_signup import login_signup
+from views.chats_parent import chats_parent
 
 app = Flask(__name__, static_folder='static')
 
@@ -9,6 +10,7 @@ app.secret_key = __import__("secrets").token_hex(16)
 
 # Register all Blueprints
 app.register_blueprint(login_signup)
+app.register_blueprint(chats_parent)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlalchemy.db"
 
@@ -19,21 +21,6 @@ migrate.init_app(app, db)
 @app.route('/')
 def index():
   return render_template('index.html')
-
-# All chats BP (child of above)
-@app.route('/chats/all')
-def all_chats():
-  return 'All chats'
-
-# Room chat BP (child of above above)
-@app.route('/chats/room/<chatname>')
-def chatroom(chatname):
-  return chatname
-
-# DM chat BP (child of above above above)
-@app.route('/chats/direct/<username>')
-def direct_chat(username):
-  return username
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080)

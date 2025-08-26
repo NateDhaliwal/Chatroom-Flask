d143a224fabd37e5ae975247bcd889642e40fffb
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-08-26 00:21:02 +0000

    Install and setup Flask-SocketIO

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask, render_template, redirect, url_for
 from flask_login import LoginManager, current_user
+from flask_socketio import SocketIO
+
 from models import db, migrate, User
 
 from views.login_signup import login_signup
@@ -26,6 +28,7 @@ login_manager = LoginManager()
 db.init_app(app)
 migrate.init_app(app, db)
 login_manager.init_app(app)
+socketio = SocketIO(app)
 
 login_manager.login_view = "login_signup.login"
 
@@ -52,3 +55,6 @@ def index():
   if current_user.is_authenticated:
     return redirect(url_for('chats_all.my_chats'))
   return render_template('index.html')
+
+if __name__ == '__main__':
+  socketio.run(app, debug=True, use_reloader=True)

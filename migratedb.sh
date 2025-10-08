82454504f489b36af7f507d5d79f1831dece3e55
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-10-08 06:55:25 +0000

    Remove old virtual environment; edit migrate file

--- migratedb.sh
+++ migratedb.sh
@@ -2,12 +2,15 @@
 
 migrate_message="$1"
 
+echo "Initiating db..."
 flask db init
 
+echo "Migrating db..."
 if [ -z "$migrate_message" ]; then
   flask db migrate
 else
   flask db migrate -m "$migrate_message"
 fi
 
+echo "Upgrading db..."
 flask db upgrade
--- migrations/versions/6c7140a9768f_.py
+++ migrations/versions/6c7140a9768f_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 37da02c6c6d6
+Revision ID: 6c7140a9768f
 Revises: 
-Create Date: 2025-08-25 04:39:44.020765
+Create Date: 2025-10-08 06:54:05.223524
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '37da02c6c6d6'
+revision = '6c7140a9768f'
 down_revision = None
 branch_labels = None
 depends_on = None
